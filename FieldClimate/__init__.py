f149dd262ecd68eec3c267cc8c1407b68f7b4ea4
Krzysztof Stopa <stopa.krzysztof.k@gmail.com>
2017-11-23 18:14:34 +0100

    Added setup script

--- FieldClimate/__init__.py
+++ FieldClimate/__init__.py
@@ -1,1 +1,6 @@
-__author__ = 'kstopa'
+__author__ = "Krzysztof Stopa"
+__copyright__ = "Copyright 2017 SatAgro & Agrimanagement"
+__credits__ = ["Krzysztof Stopa", "Przemyslaw Zelazowski", "Phillip Marshall"]
+__license__ = "LGPL"
+__email__ = "office@satagro.eu"
+__version__ = 0.3
